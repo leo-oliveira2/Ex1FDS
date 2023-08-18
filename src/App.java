31d77c44abd0631f4ba6b1867e72cc01bb26dbf6
leo-oliveira-2 <heinen126@gmail.com>
2023-08-17 23:26:25 -0300

    Tarefa 1 - inacabada

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Consultas consultas = new Consultas();
+        ConsultasRepository crv = new ConsultasRepository("poa_temps.txt");
         consultas.carregaDados();
         System.out.println("Dia em que mais choveu no ano de 1980: ");
         System.out.println(consultas.diaQueMaisChoveuNoAno(1980));
